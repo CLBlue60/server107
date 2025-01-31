ee7b26f6a76436373d2399be902ffc44b9c27010
CLBlue60 <clblue@yahoo.com>
2025-01-30 19:00:02 -0600

    assignment 3
    as stated

--- server.py
+++ server.py
@@ -1,27 +1,67 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import json
-
+from http import HTTPStatus
 
 app = Flask(__name__)
 
+# Welcome page on root endpoint
 @app.get('/')
 def home():
-    return "Hello from flask!"
+    return "Welcome to the Flask REST API!"
 
-@app.get('/test')
-def test():
-    return "This is another endpoint!"
-
-#this is a json implementation
-@app.get('/api/about')
+# About page
+@app.get('/about')
 def about():
-        name = {"name": "Blue"}
-        return json.dumps(name)
+    return """
+    <h1>About Page</h1>
+    <p>This is the about page for the Flask REST API.</p>
+    """
 
+# Sample list of products
+products = [
+    {"id": 1, "name": "Product A", "price": 10.99},
+    {"id": 2, "name": "Product B", "price": 20.99},
+]
 
+# Get all products
+@app.get('/api/product')
+def get_products():
+    return jsonify(products), HTTPStatus.OK
 
+# Get the number of products in the catalog
+@app.get('/api/product/count')
+def get_product_count():
+    return jsonify({"count": len(products)}), HTTPStatus.OK
 
-app.run(debug=True)
+# Post a new product
+@app.post('/api/product')
+def save_product():
+    product = request.get_json()
+    products.append(product)
+    return jsonify({"message": "Product saved!", "product": product}), HTTPStatus.CREATED
+
+# Delete a product by index
+@app.delete('/api/product/<int:index>')
+def delete_product(index):
+    if 0 <= index < len(products):
+        deleted_product = products.pop(index)
+        return jsonify({"message": "Product deleted!", "product": deleted_product}), HTTPStatus.OK
+    else:
+        return jsonify({"message": "Product not found"}), HTTPStatus.NOT_FOUND
+
+# Update a product by index
+@app.put('/api/product/<int:index>')
+def update_product(index):
+    product = request.get_json()
+    if 0 <= index < len(products):
+        products[index] = product
+        return jsonify({"message": "Product updated!", "product": product}), HTTPStatus.OK
+    else:
+        return jsonify({"message": "Product not found"}), HTTPStatus.NOT_FOUND
+
+# Run the Flask app
+if __name__ == '__main__':
+    app.run(debug=True)
 
 #app.post('/')
 #app.put('/')
